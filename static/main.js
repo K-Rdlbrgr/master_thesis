f936d9ba2307783046fc22d2574d2bcd41cbaf44
K-Rdlbrgr <34638@novasbe.pt>
2019-10-14 17:59:07 +0100

    Added a copy function for the private key

--- static/main.js
+++ static/main.js
@@ -8,6 +8,21 @@ function slider() {
   signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'));
 }
 
+function copyFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById('copy_privateKey');
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+  /* Copy the text inside the text field */
+  document.execCommand('copy');
+
+  /* Alert the copied text */
+  alert('Copied the text: ' + copyText.value);
+}
+
 $('#myModal').on('shown.bs.modal', function() {
   $('#myInput').focus();
 });
