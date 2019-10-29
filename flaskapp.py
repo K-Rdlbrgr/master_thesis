38b6eac1ae759c0066e57efbc251b675b3211c26
K-Rdlbrgr <34638@novasbe.pt>
2019-10-29 20:56:51 +0000

    Fixing Redis Vol.7

--- flaskapp.py
+++ flaskapp.py
@@ -40,7 +40,7 @@ login_manager.init_app(app)
 # Setting up the Session
 SESSION_TYPE = 'redis'
 r = redis.from_url(os.environ.get("REDIS_URL"))
-SESSION_REDIS = Redis(host=os.environ.get("REDIS_URL"), port=1234)
+# SESSION_REDIS = Redis(host=os.environ.get("REDIS_URL"), port=1234)
 app.config.from_object(__name__)
 Session(app)
 
